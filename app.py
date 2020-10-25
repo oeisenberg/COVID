3b0081e14de2a99976a32f6794500dcb642b0400
oeisenberg <oliver.eisenberg88@gmail.com>
2020-10-25 21:08:08 +0000

    Refactoring, add 7 day avg for line graph

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -6,6 +7,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from requests import get
+from scipy import signal
 from plotly.subplots import make_subplots
 
 def get_data(url):
@@ -30,42 +32,68 @@ def generate_table(dataframe, max_rows=10, max_cols=10):
         ])
     ], style={'marginLeft': 'auto', 'marginRight': 'auto'})
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+def generate_linegraph_cases(x, y): 
+    fig = make_subplots(specs=[[{"secondary_y": False}]])
+    fig.add_trace(
+        go.Scatter(x=x, y=y, name="Raw Data"),
+        secondary_y=False,
+    )
 
+    fig.add_trace(
+        go.Scatter(x=x, y=signal.savgol_filter(y, 7, 3), name="7 Day Average"),
+        secondary_y=False,
+    )
+
+    # fig.add_trace(
+    #     go.Scatter(x=x, y=np.diff(y), name="Rate of Change"),
+    #     secondary_y=True,
+    # )
+
+    fig.update_layout(
+        title_text="Number of COVID-19 Cases within England",
+        title_x=0.5
+    )
+    fig.update_xaxes(title_text="Time", range=[x[60],x[-1]])
+    fig.update_yaxes(title_text="Number of Cases", secondary_y=False)
+    # fig.update_yaxes(title_text="<b>secondary</b> yaxis title", secondary_y=True)
+
+    return fig
+
+def generate_piecharts_mfCases(data):
+    female_data = list(zip(*[[value.get("age"), value.get("rate"), value.get("value")] for value in data.get("data")[0].get("female")]))
+    male_data = list(zip(*[[value.get("age"), value.get("rate"), value.get("value")] for value in data.get("data")[0].get("male")]))
+
+    pies = make_subplots(rows=1, cols=3, specs=[[{'type':'domain'}, {'type':'domain'}, {'type':'domain'}]])
+    pies.add_trace(go.Pie(labels=female_data[0], values=female_data[2], hole=.3, title="Female"), 1, 1)
+    pies.add_trace(go.Pie(labels=male_data[0], values=male_data[2], hole=.3, title="Male"), 1, 2)
+    # pies.add_trace(go.Pie(labels=female_data[0], values=[female_data[2][i]-male_data[2][i] for i in range(len(female_data[2]))], hole=.3), 1, 3) # ordered by case number so need to sort
+    pies.update_layout(
+        title_text="Female and Male case numbers by age category",
+        title_x=0.5
+    )
+    return pies
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 url = (
-        'https://api.coronavirus.data.gov.uk/v1/data?'
-        'filters=areaType=nation;areaName=england&'
-        'structure={"date":"date","newCases":"newCasesByPublishDate"}'
+    'https://api.coronavirus.data.gov.uk/v1/data?'
+    'filters=areaType=nation;areaName=england&'
+    'structure={"date":"date","newCases":"newCasesByPublishDate"}'
 )
-
 data = get_data(url)
 x, y = zip(*[[value.get("date"), value.get("newCases")] for value in data.get("data")[::-1]])
+fig = generate_linegraph_cases(x, y)
+
 df = pd.DataFrame({"Dates": x, "Number of Cases": y})
-fig = px.line(df, x="Dates", y="Number of Cases")
 
 url = (
-        'https://api.coronavirus.data.gov.uk/v1/data?'
-        'filters=areaType=nation;areaName=england&'
-        'structure={"male":"maleCases","female":"femaleCases"}'
+    'https://api.coronavirus.data.gov.uk/v1/data?'
+    'filters=areaType=nation;areaName=england&'
+    'structure={"male":"maleCases","female":"femaleCases"}'
 )
-
 data = get_data(url)
-female_data = list(zip(*[[value.get("age"), value.get("rate"), value.get("value")] for value in data.get("data")[0].get("female")]))
-male_data = list(zip(*[[value.get("age"), value.get("rate"), value.get("value")] for value in data.get("data")[0].get("male")]))
-female_df = pd.DataFrame({"Age Categories": female_data[0], "rate": female_data[1], "value": female_data[2]})
-male_df = pd.DataFrame({"Age Categories": male_data[0], "rate": male_data[1], "value": male_data[2]})
-female_pie = px.pie(female_df, values='value', names='Age Categories', title='Female Cases by Age')
-male_pie = px.pie(male_df, values='value', names='Age Categories', title='Male Cases by Age')
-
-pies = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
-pies.add_trace(go.Pie(labels=female_data[0], values=female_data[2], hole=.3), 1, 1)
-pies.add_trace(go.Pie(labels=male_data[0], values=male_data[2], hole=.3), 1, 2)
-pies.update_layout(
-    title_text="Female and Male case numbers by age category",
-    title_x=0.5
-)
+pies = generate_piecharts_mfCases(data)
 
 app.layout = html.Div(children=[
     html.H1(
